7b1c98bd5476fb30829803cd795685f93cb84e3e
Julia Rulova <julia.rulova0066@gmail.com>
2022-06-20 18:01:05 +0300

    8 commit

--- routes/cards.js
+++ routes/cards.js
@@ -18,12 +18,12 @@ router.post('/cards', celebrate({
 }), createCard);
 router.put('/cards/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().hex().length(24),
+    cardId: Joi.string().hex().length(24).required(),
   }),
 }), likeCard);
 router.delete('/cards/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().hex().length(24),
+    cardId: Joi.string().hex().length(24).required(),
   }),
 }), dislikeCard);
 
