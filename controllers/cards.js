a4e3f01dae00d794937dca16fcbd9836bf4ece75
Julia Rulova <julia.rulova0066@gmail.com>
2022-06-20 18:10:36 +0300

    9 commit

--- controllers/cards.js
+++ controllers/cards.js
@@ -81,7 +81,7 @@ module.exports.likeCard = (req, res, next) => {
       res.send(card);
     })
     .catch((err) => {
-      if (err.name === 'CastError') {
+      if (err.name === 'ValidationError') {
         next(new NotValidCode('Введен некорректный id'));
       }
       next(err);
@@ -101,7 +101,7 @@ module.exports.dislikeCard = (req, res, next) => {
       res.send(card);
     })
     .catch((err) => {
-      if (err.name === 'CastError') {
+      if (err.name === 'ValidationError') {
         next(new NotValidCode('Введен некорректный id'));
       }
       next(err);
